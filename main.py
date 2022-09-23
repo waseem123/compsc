f899e89010834907e4b6379ad01a014bab0f27e4
waseem <waseemattar14@gmail.com>
2022-09-23 14:31:45 +0530

    First commit Added new pages

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import mysql.connector as connector
 app = Flask(__name__)
 
 
+def connectivity():
+    return connector.connect(host="localhost", user="root", password="", database="mpscdb")
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -13,13 +17,32 @@ def home():
 def login():
     return render_template("login.html")
 
+
 @app.route("/tables")
 def tables():
     return render_template("tables.html")
 
+@app.route("/layout")
+def layout():
+    return render_template("layout.html")
+
+@app.route("/new-allotment")
+def newallotment():
+    return render_template("layout.html")
+
+
 @app.route("/employees")
 def employees():
-    return render_template("employees.html")
+    connection = connectivity()
+    if (connection is not None):
+        sql = "SELECT `emp_id`, `emp_name`, `emp_designation`, `emp_organization`, `emp_mobileno` FROM `tbl_employee`"
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        # print(result)
+    return render_template("employees.html", employeelist=result)
+
 
 @app.route("/exam-centers")
 def examcenters():
